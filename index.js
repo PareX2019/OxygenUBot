ebfefea1458dfe3003f1b4e4e246dd8f53d5ed60
PareX <54591153+PareX2019@users.noreply.github.com>
2020-08-31 16:02:20 +0200

    testing

--- index.js
+++ index.js
@@ -19,12 +19,47 @@ Bot.once('ready', () => {
     console.log(`${Bot.user.username} is now Running.`);
 });
 
+
+async function registerCommands(message,args,dataALL,data){
+    const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+    commandFiles.forEach(command2 =>{
+     const big = command2.replace(".js"," ");
+
+     if(command === big){
+      Bot.commands.get(big).execute(message,args,dataALL,data);
+     }
+     console.log(big);
+     
+  })
+}
+
 Bot.on('message' , async message =>{
 
-    if(message.author.bot)
-    {
-        return;
+    if(message.author.bot || message.member.roles.find("710416627409223737")||message.member.roles.find("710416627409223738")||message.member.roles.find("741734635674927114")||member.roles.find("710416627413549127")||member.roles.find("710416627413549128")){
+       return;
     }
+    else
+    {
+        if(message.content.includes("help")||message.content.includes("Help")||message.content.includes("HELP")){
+            if (talkedRecently.has(message.author.id)) {
+                return;
+        } else {
+            if(message.channel.parentID === '710416628206010426'){
+                const ChannelName = Bot.channels.cache.get("744650457506512896");
+             message.reply(`You Can Get Oxygen U Related Help In ${ChannelName.toString()} By Reacting With The 📩 Emoji :)`);
+            }
+            else
+            {
+                return;
+            }
+            talkedRecently.add(message.author.id);
+            setTimeout(() => {
+              talkedRecently.delete(message.author.id);
+            }, 600000);
+        }
+        }
+    }
+   
 
     const args = message.content.slice(process.env.prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
@@ -32,51 +67,9 @@ Bot.on('message' , async message =>{
     const data = await api.countries({country: args});
     const dataALL = await api.all();
 
-    if(message.content.includes("help")||message.content.includes("Help")||message.content.includes("HELP")){
-        if (talkedRecently.has(message.author.id)) {
-            return;
-    } else {
-        if(message.channel.parentID === '710416628206010426'){
-            const ChannelName = Bot.channels.cache.get("744650457506512896");
-         message.reply(`You Can Get Oxygen U Related Help In ${ChannelName.toString()} By Reacting With The 📩 Emoji :)`);
-        }
-        else
-        {
-            return;
-        }
-        talkedRecently.add(message.author.id);
-        setTimeout(() => {
-          talkedRecently.delete(message.author.id);
-        }, 600000);
-    }
-    }
-
     if(!message.content.startsWith(process.env.prefix) || message.author.bot) return;
     
-    if(command === 'ping'){
-        Bot.commands.get('ping').execute(message, args);
-    }
-    else if(command === 'download'){
-        Bot.commands.get('download').execute(message, args);
-    }
-    else if(command === 'covid'){
-        Bot.commands.get('covid').execute(message,args,data,dataALL);
-    }
-    else if(command === 'oxy_api'){
-        Bot.commands.get('oxy_api').execute(message,args);
-    }
-    else if(command === 'log_commands'){
-        if(message.author.id === '503471433415000079'){
-    const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-      commandFiles.forEach(command2 =>{
-       const big = command2.replace(".js"," ");
-       console.log(big);
-      });
-        }
-        else{
-            return;
-        }
-    }
+    registerCommands(message,args,dataALL,data)
 });
 
 
