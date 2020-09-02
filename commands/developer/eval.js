da4040f4f9ce3355d8c346cad144eb0986a6e61f
PareX <54591153+PareX2019@users.noreply.github.com>
2020-09-02 19:09:28 +0200

    a

--- commands/developer/eval.js
+++ commands/developer/eval.js
@@ -6,12 +6,12 @@ module.exports = {
     description: "Allows the developers to run JavaScript code.",
     run: async (client, message, args) => {
         if(!args[0]){
-            let EmbedYes = new discord.MessageEmbed()
+            const EmbedYes = new discord.MessageEmbed()
             .setColor('#00a9be')
             .setTimestamp()
             .setTitle("Oxgygen U")
             .addField("Usage: `~eval [code]`")
-            .addFooter(`Command Run By ${message.author.username}`,message.author.avatarURL)
+            .addFooter(`Command Run By ${message.author.username}`,message.author.avatarURL);
             
              message.channel.send(EmbedYes);
         } 
@@ -19,30 +19,30 @@ module.exports = {
         try {
             let evaluated = inspect(eval(code, {depth: 0}));
             if(evaluated.includes(client.token) && code.includes(client.token)){
-                let monkEmbed = new discord.MessageEmbed()
+                const monkEmbed = new discord.MessageEmbed()
                 .setColor('#00a9be')
                 .setTimestamp()
                 .setTitle("Oxgygen U")
                 .addField(`Some Monkey Called ${message.author.username} Just Tried To Get ${client.user.username} Token.`)
-                .addFooter(`Command Run By ${message.author.username}`,message.author.avatarURL)
+                .addFooter(`Command Run By ${message.author.username}`,message.author.avatarURL);
 
                 client.channels.get('750704480433078352').send(monkEmbed);
             }
-            let CorrectEmbed = new discord.MessageEmbed()
+            const CorrectEmbed = new discord.MessageEmbed()
             .setColor('#00a9be')
             .setTimestamp()
             .setTitle("Oxgygen U")
             .addField(`Ran the code. Output: \`\`\`js ${evaluated}\`\`\``)
-            .addFooter(`Command Run By ${message.author.username}`,message.author.avatarURL)
+            .addFooter(`Command Run By ${message.author.username}`,message.author.avatarURL);
 
             message.channel.send(CorrectEmbed);
         } catch(err) {
-            let errorEmbed = new discord.MessageEmbed()
+            const errorEmbed = new discord.MessageEmbed()
             .setColor('#00a9be')
             .setTimestamp()
             .setTitle("Oxgygen U")
             .addField(`Code failed to run. Output: \`\`\`${err.message}\`\`\``)
-            .addFooter(`Command Run By ${message.author.username}`,message.author.avatarURL)
+            .addFooter(`Command Run By ${message.author.username}`,message.author.avatarURL);
             
             message.channel.send(errorEmbed);
         }
